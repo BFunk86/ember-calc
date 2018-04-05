670bf079852d0cbc0b55160dd5af9bd1b1384551
Brandon Young <byoung@deepbluecommunications.com>
2018-04-05 01:11:33 -0400

    Removed unused code from the component

--- app/components/calculator-screen.js
+++ app/components/calculator-screen.js
@@ -1,13 +1,4 @@
 import Component from '@ember/component';
 
-
-let answer = '';
-
 export default Component.extend({
-  actions: {
-    calculate() {
-      answer = eval(this.$("#answer").val());
-      this.$("#answer").val(answer);
-    }
-  }
 });
